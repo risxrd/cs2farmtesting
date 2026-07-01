ac0f1284fe8a5ddec3eb621304e4e8c3245ef23e
ㅤㅤㅤㅤㅤㅤ <sdkuralsiz@gmail.com>
2026-07-02 01:00:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,25 @@ const SteamTotp = require('steam-totp');
 const express = require('express');
 require('dotenv').config();
 
-// Railway'in projeyi uyku moduna almasını engellemek için mini web portu
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('CS2 Saat Kasma Sistemi Aktif!'));
-app.listen(port, () => console.log(`Sunucu ${port} portunda dinleniyor.`));
+app.get('/', (req, res) => res.send('Aktif'));
+app.listen(port);
 
 const client = new SteamUser();
 
-const logOnOptions = {
+client.logOn({
     accountName: process.env.ardab2134,
     password: process.env.1905ardaM1907,
-    twoFactorCode: SteamTotp.generateAuthCode(process.env.XN8YQ)
-};
-
-client.logOn(logOnOptions);
-
-client.on('loggedOn', () => {
-    console.log('✅ Steam hesabına başarıyla giriş yapıldı.');
-    client.setPersona(SteamUser.EPersonaState.Online);
-    
-    // Sadece CS2
-    client.gamesPlayed([730]); 
-    console.log('CS2 saat kasma işlemi başladı...');
+    twoFactorCode: SteamTotp.generateAuthCode(process.env.G255W)
 });
 
-client.on('disconnected', (eresult, msg) => {
-    console.log(`⚠️ Bağlantı koptu: ${msg}. Tekrar deneniyor...`);
+client.on('loggedOn', () => {
+    console.log('Giris basarili');
+    client.setPersona(1);
+    client.gamesPlayed([730]);
 });
 
 client.on('error', (err) => {
-    console.error(`❌ Hata: ${err.message}`);
+    console.log('Hata:', err);
 });
